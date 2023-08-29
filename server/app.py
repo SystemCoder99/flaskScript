e4a6195a2863c616d85134ffe063e562bf985150
SystemCoder99 <mayjay1999@googlemail.com>
2023-08-29 11:25:30 +0100

    added GET functionality

--- server/app.py
+++ server/app.py
@@ -8,11 +8,37 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'The Lightning Thief',
+        'author': 'Rick Riordan',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 
 # sanity check route
 @app.route("/ping", methods=["GET"])
 def ping_pong():
     return jsonify("Pong!")
+
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
 
 
 if __name__ == "__main__":
